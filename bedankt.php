4adb69ec7bd2233939c7b4c51c357a31770613fc
SeanNeerings <seanneerings1@gmail.com>
2022-04-28 10:16:37 +0200

    bedankt page

---
+++ bedankt.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <header>
+        <link rel="stylesheet" href="style.css">
+        <div class="header">
+            <img src="assets/img/logo-small.svg" alt="vrijwilligershuis">
+            <div class="text">
+                <p>VrijwilligersHuis</p>
+                <p>Nieuwegein </p>
+            </div>
+
+        </div>
+
+        <nav></nav>
+        <p>Menu</p>
+        <p>Account aanmaken</p>
+        <p>Inloggen</p>
+        </nav>
+
+    </header>
+
+    <div id="container">
+        <div class="bedankt">
+            <h1> Badankt voor u donatie</h1>
+            <h3> Lorem ipsum, dolor sit amet consectetur adipisicing elit. Non sapiente amet voluptate suscipit. Necessitatibus, maiores dolorum suscipit quam tempore porro dicta assumenda aliquid. Nobis beatae repellat non veritatis, officiis quas. Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime assumenda culpa ea. Impedit sit dolores odit ducimus omnis voluptatum, iusto nemo autem ullam mollitia est beatae, numquam sequi! Maiores, pariatur.</h3>
+            <a href="index.php">  Ga terug naar de hoofd pagina</a>
+        </div>
+    </div>
+</body>
+</html>

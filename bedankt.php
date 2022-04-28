5d4a8cb9a5d02bfdc8a85b95671744d9e7a3af49
CasMeijer2005 <90309791+CasMeijer2005@users.noreply.github.com>
2022-04-28 10:40:34 +0200

    update

--- bedankt.php
+++ bedankt.php
@@ -1,14 +1,20 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- Latest compiled JavaScript -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"></script>
 </head>
+
 <body>
     <header>
-        <link rel="stylesheet" href="style.css">
+        <link rel="stylesheet" href="./assets/css/style.css">
         <div class="header">
             <img src="assets/img/logo-small.svg" alt="vrijwilligershuis">
             <div class="text">
@@ -30,8 +36,9 @@
         <div class="bedankt">
             <h1> Badankt voor u donatie</h1>
             <h3> Lorem ipsum, dolor sit amet consectetur adipisicing elit. Non sapiente amet voluptate suscipit. Necessitatibus, maiores dolorum suscipit quam tempore porro dicta assumenda aliquid. Nobis beatae repellat non veritatis, officiis quas. Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime assumenda culpa ea. Impedit sit dolores odit ducimus omnis voluptatum, iusto nemo autem ullam mollitia est beatae, numquam sequi! Maiores, pariatur.</h3>
-            <a href="index.php">  Ga terug naar de hoofd pagina</a>
+            <button href="index.php" type="button" class="btn purple text-white">terug naar homepage</button>
         </div>
     </div>
 </body>
+
 </html>
